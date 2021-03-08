e337572a320d02d9e5bc3f4659b0f7836ef25e55
vinicius pizetta de souza <viniciuspzt@gmail.com>
2021-03-08 03:24:36 -0300

    fix part of rubocop offenses

--- bot_saves_princess_two.rb
+++ bot_saves_princess_two.rb
@@ -1,38 +1,50 @@
 #!/bin/ruby
+# frozen_string_literal: true
 
-def next_move(n, r, c, grid)
+def next_move(_size, row, col, grid)
   princess = { x: 0, y: 0 }
-  mario = { x: c, y: r }
+  mario = { x: col, y: row }
 
-  two_dimensional = grid.map { |i| i.split(%r{\s*}) }
+  two_dimensional = grid.map { |i| i.split(/\s*/) }
 
-  two_dimensional.map.with_index do |row, row_index|
-    row.map.with_index do |col, col_index|
-      if (col == 'p')
-        princess[:x] = col_index
-        princess[:y] = row_index
+  princess_position = get_position(two_dimensional, 'p')
+  princess[:x] = princess_position.first
+  princess[:y] = princess_position.last
+  move_mario(mario, princess)
+end
+
+def get_position(grid_2d, initial)
+  x, y = 0
+  grid_2d.map.with_index do |row_2d, row_index|
+    row_2d.map.with_index do |col_2d, col_index|
+      if col_2d == initial
+        x = col_index
+        y = row_index
       end
     end
   end
+  [x, y]
+end
 
+def move_mario(mario, princess)
   moved = false
-  until moved == true
-    if mario[:y] < princess[:y] && moved == false
+  until moved
+    if mario[:y] < princess[:y] && !moved
       mario[:y] += 1
       puts('DOWN')
       moved = true
     end
-    if mario[:y] > princess[:y] && moved == false
+    if mario[:y] > princess[:y] && !moved
       mario[:y] -= 1
       puts('UP')
       moved = true
     end
-    if mario[:x] < princess[:x] && moved == false
+    if mario[:x] < princess[:x] && !moved
       mario[:x] += 1
       puts('RIGHT')
       moved = true
     end
-    if mario[:x] > princess[:x] && moved == false
+    if mario[:x] > princess[:x] && !moved
       mario[:x] -= 1
       puts('LEFT')
       moved = true
@@ -42,7 +54,7 @@ end
 
 n = gets.to_i
 
-r, c = gets.strip.split.map { |num| num.to_i }
+row, column = gets.strip.split.map(&:to_i)
 
 grid = Array.new(n)
 
@@ -50,4 +62,4 @@ grid = Array.new(n)
   grid[i] = gets
 end
 
-next_move(n, r, c, grid)
+next_move(n, row, column, grid)
